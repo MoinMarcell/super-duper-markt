981165e3334b0f9a0546cc42cbeeb980588728e6
Marcell <marcell.dechant@protonmail.com>
2024-02-05 15:57:17 +0100

    Refactor Product test instances and add new test

--- src/test/java/com/github/moinmarcell/superdupermarkt/product/ProductManagerTest.java
+++ src/test/java/com/github/moinmarcell/superdupermarkt/product/ProductManagerTest.java
@@ -13,37 +13,36 @@ class ProductManagerTest {
     private final ProductRepository productRepository = mock(ProductRepository.class);
     private final ProductManager productManager = new ProductManager(productRepository);
 
-    Product cheese = new Product("Gouda", ProductType.CHEESE, new BigDecimal("1.99"), 50, LocalDate.of(2024, 3, 28),
+    private static final Product cheese = new Product("Gouda", ProductType.CHEESE, new BigDecimal("1.99"), 50,
+            LocalDate.of(2024, 3, 28),
             LocalDate.of(2024, 2, 2));
 
-    Product wine = new Product("Merlot", ProductType.WINE, new BigDecimal("15.00"), 20, null, LocalDate.of(2024, 1, 20));
+    private static final Product wine = new Product("Merlot", ProductType.WINE, new BigDecimal("15.00"), 20, null, LocalDate.of(2024, 1, 20));
 
-    Product bread = new Product("Bread", ProductType.BREAD, new BigDecimal("2.00"), 15, LocalDate.of(2024, 2, 6),
+    private static final Product bread = new Product("Bread", ProductType.BREAD, new BigDecimal("2.00"), 15, LocalDate.of(2024, 2, 6),
             LocalDate.of(2024, 1, 28));
 
     @Test
     void checkQualityShouldReturnTrueForCheeseWithQuality30() {
-        cheese = new Product("Gouda", ProductType.CHEESE, new BigDecimal("1.99"), 30, LocalDate.of(2024, 3, 28),
-                LocalDate.of(2024, 2, 2));
         assertTrue(productManager.checkQuality(cheese));
     }
 
     @Test
     void checkQualityShouldReturnFalseForCheeseWithQuality29() {
-        cheese = new Product("Gouda", ProductType.CHEESE, new BigDecimal("1.99"), 29, LocalDate.of(2024, 3, 28),
+        Product cheese = new Product("Gouda", ProductType.CHEESE, new BigDecimal("1.99"), 29,
+                LocalDate.of(2024, 3, 28),
                 LocalDate.of(2024, 2, 2));
         assertFalse(productManager.checkQuality(cheese));
     }
 
     @Test
     void checkQualityShouldReturnTrueForWineWithQuality1() {
-        wine = new Product("Merlot", ProductType.WINE, new BigDecimal("15.00"), 1, null, LocalDate.of(2024, 1, 20));
         assertTrue(productManager.checkQuality(wine));
     }
 
     @Test
     void checkQualityShouldReturnFalseForWineWithQuality0() {
-        wine = new Product("Merlot", ProductType.WINE, new BigDecimal("15.00"), 0, null, LocalDate.of(2024, 1, 20));
+        Product wine = new Product("Merlot", ProductType.WINE, new BigDecimal("15.00"), 0, null, LocalDate.of(2024, 1, 20));
         assertFalse(productManager.checkQuality(wine));
     }
 
@@ -55,6 +54,11 @@ class ProductManagerTest {
     @Test
     void isExpiredShouldReturnFalseForNotExpiredProduct() {
         assertFalse(productManager.isExpired(bread, LocalDate.of(2024, 2, 5)));
+    }
+
+    @Test
+    void isExpiredShouldReturnFalseForProductWithNoExpirationDate() {
+        assertFalse(productManager.isExpired(bread, LocalDate.of(2024, 2, 6)));
     }
 
     @Test
